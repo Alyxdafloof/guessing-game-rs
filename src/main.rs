e37084149b07d17b9977924b54478934c86baf6b
alyxdafloof <Auroraholtman@gmail.com>
2024-11-29 13:35:39 -0600

    Feature: user input functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+//Guessing Game//
+//Libraries Used//
+use std::io;
+//Functions
+fn main () {
+    println!("Hello user, please enter a number~");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Cant read line");
+    let guess: u32 = guess.trim().parse().expect("couldnt trim and parse");
+
+    println!("user guess {}", guess);
 }
