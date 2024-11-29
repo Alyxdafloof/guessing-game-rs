3d0a859657f3d6722b0322963648265653ef7882
alyxdafloof <Auroraholtman@gmail.com>
2024-11-29 14:05:09 -0600

    Features: Secret Number generation, User input comparison, and Win condition

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
 //Guessing Game//
 //Libraries Used//
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 //Functions
 fn main () {
-    println!("Hello user, please enter a number~");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Cant read line");
-    let guess: u32 = guess.trim().parse().expect("couldnt trim and parse");
+//calles a new secret number variable using the rand library
+//generates the secret_number using an INCLUSIVE range from 1 - 100
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("user guess {}", guess);
+//Prompts user for input
+//starts gameplay loop
+//calls new string var (guess)
+//writes user input to guess with a reference as to not take ownership
+//then makes guess a u32 int by triming and parsing it, also now making it immutible
+    println!("Hello user, please enter a number~");
+
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Cant read line");
+        let guess: u32 = guess.trim().parse().expect("couldnt trim and parse");
+
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("Perfect!");
+                break;
+            },
+        }
+    }
 }
